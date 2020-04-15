d58e817a2eb84f751039037e1c7ab3f3702d88be
Max Veregge <maxwell.veregge@gmail.com>
2020-04-15 19:34:15 -0400

    refactor covid data to live on Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,24 +3,74 @@ import GoogleMapReact from "google-map-react";
 import MapMarker from "./MapMarker.js";
 import MapSettings from "./MapSettings.js";
 import { bootstrapURLkey } from "../bootstrapURLkey.js";
+import centroidJson from "../content/centroids.json";
 
 class Map extends React.Component {
-  static defaultProps = {
-    center: { lat: 40.73, lng: -73.93 },
-    zoom: 12,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      covidData: {},
+      currentCountry: "",
+      center: { lat: 0, lng: 0 },
+      zoom: 0,
+    };
+    this.handleCountryChange = this.handleCountryChange.bind(this);
+  }
+
+  handleCountryChange(event) {
+    this.setState({ currentCountry: event.target.value });
+  }
+
+  componentDidMount() {
+    const centroidReducer = (obj, centroid) => {
+      obj[centroid["country"]] = {
+        lat: centroid["latitude"],
+        lng: centroid["longitude"],
+      };
+      return obj;
+    };
+    const centroids = centroidJson.reduce(centroidReducer, {});
+
+    const dataReducer = (obj, data) => {
+      obj[data["CountryCode"]] = {
+        name: data["Country"],
+        totalConfirmed: data["TotalConfirmed"],
+        totalDeaths: data["TotalDeaths"],
+        totalRecovered: data["TotalRecovered"],
+      };
+      return obj;
+    };
+    fetch("https://api.covid19api.com/summary").then((data) => {
+      let formattedData;
+      data.json().then((json) => {
+        formattedData = json["Countries"].reduce(dataReducer, {});
+        for (var countryCode in formattedData) {
+          if (!(typeof centroids[countryCode] === "undefined")) {
+            formattedData[countryCode]["centroid"] = centroids[countryCode];
+          }
+        }
+        this.setState((state, props) => ({
+          covidData: formattedData,
+        }));
+      });
+    });
+  }
   render() {
     return (
       <>
-        <MapSettings />
+        <MapSettings
+          countries={this.state.covidData}
+          currentCountry={this.state.currentCountry}
+          handleCountryChange={this.handleCountryChange}
+        />
         <GoogleMapReact
           style={{ height: "100vh", width: "100vh" }}
           bootstrapURLKeys={{
             key: bootstrapURLkey,
             language: "en",
           }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
         >
           <MapMarker {...this.props.center} text="100" />
         </GoogleMapReact>
--- src/components/MapSettings.js
+++ src/components/MapSettings.js
@@ -1,63 +1,19 @@
 import React from "react";
 import { mapSettingsStyle } from "../styles/MapSettingsStyle.js";
-import centroidJson from "../content/centroids.json";
 
 class MapSettings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      countries: [{ name: "Select A Country", code: "", centroid: null }],
-      currentCountry: "",
-    };
-    this.handleCountryChange = this.handleCountryChange.bind(this);
-  }
-
-  componentDidMount() {
-    const centroidReducer = (obj, centroid) => {
-      obj[centroid["country"]] = {
-        lat: centroid["latitude"],
-        lng: centroid["longitude"],
-      };
-      return obj;
-    };
-    const centroids = centroidJson.reduce(centroidReducer, {});
-
-    fetch("https://api.covid19api.com/countries").then((data) => {
-      let availableCountries;
-      data.json().then((json) => {
-        availableCountries = json.map((country) => {
-          return { name: country.Country, code: country.ISO2 };
-        });
-        availableCountries.forEach((country, index) => {
-          if (!(typeof centroids[country["code"]] === "undefined")) {
-            availableCountries[index]["centroid"] = centroids[country["code"]];
-          }
-        });
-        this.setState((state, props) => ({
-          countries: state.countries.concat(availableCountries),
-        }));
-      });
-    });
-  }
-
-  handleCountryChange(event) {
-    this.setState({ currentCountry: event.target.value });
-  }
-
   render() {
-    let countries = this.state.countries;
-
     return (
       <div>
         <select
-          value={this.state.currentCountry}
-          onChange={this.handleCountryChange}
+          value={this.props.currentCountry}
+          onChange={this.props.handleCountryChange}
           style={mapSettingsStyle}
         >
-          {countries.map((country) => {
+          {Object.keys(this.props.countries).map((countryCode) => {
             return (
-              <option value={country.code} key={country.code}>
-                {country.name}
+              <option value={countryCode} key={countryCode}>
+                {this.props.countries[countryCode]["name"]}
               </option>
             );
           })}
