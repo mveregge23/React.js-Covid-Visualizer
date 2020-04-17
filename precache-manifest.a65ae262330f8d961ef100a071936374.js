72f05b42285226eb9acf0c237e76e6bb957ef2e3
Max Veregge <maxwell.veregge@gmail.com>
2020-04-17 16:01:09 -0400

    Updates

---
+++ precache-manifest.a65ae262330f8d961ef100a071936374.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1441487ccbd683a48787530fd4b828f",
+    "url": "/React.js-covid-visualizer/index.html"
+  },
+  {
+    "revision": "25338366ccb91827ecae",
+    "url": "/React.js-covid-visualizer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "98e4350f4cd80e043a06",
+    "url": "/React.js-covid-visualizer/static/css/main.c7446496.chunk.css"
+  },
+  {
+    "revision": "25338366ccb91827ecae",
+    "url": "/React.js-covid-visualizer/static/js/2.c6ea4f02.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/React.js-covid-visualizer/static/js/2.c6ea4f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e4350f4cd80e043a06",
+    "url": "/React.js-covid-visualizer/static/js/main.828b81a6.chunk.js"
+  },
+  {
+    "revision": "b8423b2a7f3fbd0b428c",
+    "url": "/React.js-covid-visualizer/static/js/runtime-main.07e968ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React.js-covid-visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
